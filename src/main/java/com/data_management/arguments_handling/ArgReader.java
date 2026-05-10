d803480ff4e4e453b49ccc31be9284df71c74b3a
perumames <perumames@gmail.com>
2026-05-10 17:20:55 +0200

    Remove argument parsing, unnecessary functionality

--- src/main/java/com/data_management/arguments_handling/ArgReader.java
+++
@@ -1,8 +0,0 @@
-package com.data_management.arguments_handling;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface ArgReader {
-    Map<String, String> readArguments() throws IOException;
-}
--- src/main/java/com/data_management/file_reading/FileParser.java
+++ src/main/java/com/data_management/file_reading/FileParser.java
@@ -1,26 +1,36 @@
 package com.data_management.file_reading;
 
 import com.data_management.DataStorage;
-import com.data_management.arguments_handling.ArgParser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileParser implements DataReader {
 
     String path;
-    ArgParser reader;
 
     public FileParser(String[] args) throws IOException {
         if(args.length == 0) {
             throw new IOException("File path empty or just spaces");
         }
-        reader = new ArgParser(args);
-        this.path = reader.getPath();
-
     }
 
+    // addPatientData(int patientId, double measurementValue, String recordType, long timestamp)
+    // "Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n"
     @Override
-    public void readData(DataStorage dataStorage) {
+    public void readData(DataStorage dataStorage) throws FileNotFoundException {
+        File file = new File(path);
+        if(!file.exists()) {
+            throw new FileNotFoundException(path + " does not exist");
+        }
 
+        Scanner scanner = new Scanner(file);
+
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line.split(", ");
+        }
     }
 }
