8aca98a83605a4814721864cf280fe5488e91aaf
perumames <perumames@gmail.com>
2026-05-10 15:19:01 +0200

    Uncomment suggested lines for DataReader implementation and create constructor for FileParser

--- src/main/java/com/data_management/DataStorage.java
+++ src/main/java/com/data_management/DataStorage.java
@@ -1,5 +1,6 @@
 package com.data_management;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,8 +15,6 @@ import com.alerts.AlertGenerator;
  */
 public class DataStorage {
     private Map<Integer, Patient> patientMap; // Stores patient objects indexed by their unique patient ID.
-
-    String[] args;
 
     /**
      * Constructs a new instance of DataStorage, initializing the underlying storage
@@ -84,15 +83,14 @@ public class DataStorage {
      * 
      * @param args command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // DataReader is not defined in this scope, should be initialized appropriately.
-        // DataReader reader = new SomeDataReaderImplementation("path/to/data");
+        DataReader reader = new FileParser("path/to/data");
         DataStorage storage = new DataStorage();
-        storage.args = args;
 
         // Assuming the reader has been properly initialized and can read data into the
         // storage
-        // reader.readData(storage);
+        reader.readData(storage);
 
         // Example of using DataStorage to retrieve and print records for a patient
         List<PatientRecord> records = storage.getRecords(1, 1700000000000L, 1800000000000L);
--- src/main/java/com/data_management/FileParser.java
+++ src/main/java/com/data_management/FileParser.java
@@ -3,8 +3,15 @@ package com.data_management;
 import java.io.IOException;
 
 public class FileParser implements DataReader {
+
+    public FileParser(String path) throws IOException {
+        if(path.trim().isEmpty()) {
+            throw new IOException("File path empty or just spaces");
+        }
+    }
+
     @Override
-    public void readData(DataStorage dataStorage) throws IOException {
+    public void readData(DataStorage dataStorage) {
 
     }
 }
