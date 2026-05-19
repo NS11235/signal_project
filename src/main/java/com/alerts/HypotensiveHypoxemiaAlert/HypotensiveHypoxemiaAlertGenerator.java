32938e1fb44b2da215630684ff75ac0deeed3e69
perumames <perumames@gmail.com>
2026-05-19 16:05:17 +0200

    Add HypotensiveHypoxemiaAlertGenerator

---
+++ src/main/java/com/alerts/HypotensiveHypoxemiaAlert/HypotensiveHypoxemiaAlertGenerator.java
@@ -0,0 +1,48 @@
+package com.alerts.HypotensiveHypoxemiaAlert;
+
+import com.alerts.Alert;
+import com.data_management.Patient;
+import com.data_management.PatientRecord;
+
+import java.util.List;
+
+public class HypotensiveHypoxemiaAlertGenerator {
+
+    public void evaluateData(Patient patient, List<PatientRecord> records) {
+        PatientRecord latestSystolic = null;
+        PatientRecord latestSaturation = null;
+
+        // Records latest records of systolic and saturation alerts
+        for (PatientRecord record : records) {
+            if (record.getRecordType().contains("SystolicPressure")) {
+                latestSystolic = record;
+            } else if (record.getRecordType().equals("BloodSaturation")) {
+                latestSaturation = record;
+            }
+        }
+
+        // Patient could have no systolic or saturation alerts, making this alert impossible
+        if (latestSystolic == null || latestSaturation == null) {
+            return;
+        }
+
+        // Calculates time difference for alerts to tripper Hypotensive Hypoxemia alert
+        long timeDiff = Math.abs(latestSystolic.getTimestamp() - latestSaturation.getTimestamp());
+        long tenMinutes = 10 * 60 * 1000L;
+
+        // If both are under the threshold at the same time, triggers alert
+        if (timeDiff < tenMinutes
+                && latestSystolic.getMeasurementValue() < 90
+                && latestSaturation.getMeasurementValue() < 92) {
+            triggerAlert(new HypotensiveHypoxemiaAlert(
+                    String.valueOf(patient.getPatientId()),
+                    "Low Systolic Pressure and Blood Saturation, Hypotensive Hypoxemia Alert",
+                    System.currentTimeMillis()
+            ));
+        }
+    }
+
+    private void triggerAlert(Alert alert) {
+
+    }
+}
