37646618b33f3ec333e8294d142cfc1f10ca096b
perumames <perumames@gmail.com>
2026-05-21 16:43:38 +0200

    Finish partitioned unit test for HypotensiveHypoxemiaAlertGenerator

--- src/test/java/Alerts/AlertGenerators/HypotensiveHypoxemiaAlertGeneratorTest.java
+++ src/test/java/Alerts/AlertGenerators/HypotensiveHypoxemiaAlertGeneratorTest.java
@@ -23,4 +23,49 @@ public class HypotensiveHypoxemiaAlertGeneratorTest {
         assert alert.getCondition().equals("Low Systolic Pressure and Blood Saturation, Hypotensive Hypoxemia Alert");
         assert alert.getTimestamp() <= System.currentTimeMillis();
     }
+
+    @Test
+    void testSystolicConditionMet() {
+        Patient patient = new Patient(1);
+        long currentTime = System.currentTimeMillis();
+        patient.addRecord(89, "SystolicPressure", currentTime);
+        patient.addRecord(100, "BloodSaturation", currentTime);
+
+        List<PatientRecord> records = patient.getRecords(0, System.currentTimeMillis());
+        HypotensiveHypoxemiaAlertGenerator generator = new HypotensiveHypoxemiaAlertGenerator();
+        Alert alert = generator.evaluateData(patient, records);
+        assert Integer.parseInt(alert.getPatientId()) == 0;
+        assert alert.getCondition().equals("no condition");
+        assert alert.getTimestamp() <= System.currentTimeMillis();
+    }
+
+    @Test
+    void testBloodSaturationConditionMet() {
+        Patient patient = new Patient(1);
+        long currentTime = System.currentTimeMillis();
+        patient.addRecord(100, "SystolicPressure", currentTime);
+        patient.addRecord(91, "BloodSaturation", currentTime);
+
+        List<PatientRecord> records = patient.getRecords(0, System.currentTimeMillis());
+        HypotensiveHypoxemiaAlertGenerator generator = new HypotensiveHypoxemiaAlertGenerator();
+        Alert alert = generator.evaluateData(patient, records);
+        assert Integer.parseInt(alert.getPatientId()) == 0;
+        assert alert.getCondition().equals("no condition");
+        assert alert.getTimestamp() <= System.currentTimeMillis();
+    }
+
+    @Test
+    void testNoConditionsMet() {
+        Patient patient = new Patient(1);
+        long currentTime = System.currentTimeMillis();
+        patient.addRecord(100, "SystolicPressure", currentTime);
+        patient.addRecord(100, "BloodSaturation", currentTime);
+
+        List<PatientRecord> records = patient.getRecords(0, System.currentTimeMillis());
+        HypotensiveHypoxemiaAlertGenerator generator = new HypotensiveHypoxemiaAlertGenerator();
+        Alert alert = generator.evaluateData(patient, records);
+        assert Integer.parseInt(alert.getPatientId()) == 0;
+        assert alert.getCondition().equals("no condition");
+        assert alert.getTimestamp() <= System.currentTimeMillis();
+    }
 }
