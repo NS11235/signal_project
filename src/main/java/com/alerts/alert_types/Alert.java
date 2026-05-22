371f97bcb9486566004f86c52485007d6cd0f49b
NS11235 <n.simionescu@student.maastrichtuniversity.nl>
2026-05-22 23:34:17 +0200

    Update Alert.java

--- src/main/java/com/alerts/alert_types/Alert.java
+++ src/main/java/com/alerts/alert_types/Alert.java
@@ -1,7 +1,18 @@
 package com.alerts.alert_types;
-
+/**
+ * Interface for all alert types.
+ */
 public interface Alert {
+     /**
+     * Returns the patient ID.
+     */
     int getPatientId();
+     /**
+     * Returns the alert condition.
+     */
     String getCondition();
+     /**
+     * Returns the alert timestamp.
+     */
     long getTimestamp();
 }
