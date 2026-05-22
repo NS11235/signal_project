5a6df6d7e816d53841bb9ecf0db0a79a7f439f31
NS11235 <n.simionescu@student.maastrichtuniversity.nl>
2026-05-22 20:04:50 +0200

    Update ECGAlertFactory.java

--- src/main/java/com/alerts/alert_types/alert_factories/ECGAlertFactory.java
+++ src/main/java/com/alerts/alert_types/alert_factories/ECGAlertFactory.java
@@ -1,11 +1,12 @@
 package com.alerts.alert_types.alert_factories;
 
 import com.alerts.alert_types.AlertClassic;
+import com.alerts.alert_types.ECGAlert;
 
 public class ECGAlertFactory extends AlertFactory {
     @Override
-    public AlertClassic createAlert(int patientId, String condition, long timestamp)
+    public AlertClassic createAlert(int patientId, String condition, long timestamp, double value)
     {
-        return new AlertClassic(patientId, "ECG" + condition, timestamp);
+        return new ECGAlert(patientId, "ECG" + condition, timestamp);
     }
 }
