78498a70cce4063973fdb1b175e7d29ce02c97a0
NS11235 <n.simionescu@student.maastrichtuniversity.nl>
2026-05-22 23:39:39 +0200

    Update TriggeredAlert.java

--- src/main/java/com/alerts/alert_types/TriggeredAlert.java
+++ src/main/java/com/alerts/alert_types/TriggeredAlert.java
@@ -1,5 +1,7 @@
 package com.alerts.alert_types;
-
+/**
+ * Alert triggered by a cause.
+ */
 public class TriggeredAlert extends AlertClassic implements Alert{
 
     public TriggeredAlert(int patientId, long timestamp) {
