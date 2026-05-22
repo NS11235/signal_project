fd548694315888643d0e73850f458602b854bdd5
NS11235 <n.simionescu@student.maastrichtuniversity.nl>
2026-05-22 20:05:47 +0200

    Add files via upload

---
+++ src/main/java/com/alerts/alert_types/alert_factories/HypotensiveHypoxemiaAlertFactory.java
@@ -0,0 +1,12 @@
+package com.alerts.alert_types.alert_factories;
+
+import com.alerts.alert_types.AlertClassic;
+import com.alerts.alert_types.HypotensiveHypoxemiaAlert;
+
+public class HypotensiveHypoxemiaAlertFactory extends AlertFactory {
+    @Override
+    public AlertClassic createAlert(int patientId, String condition, long timestamp,double value)
+    {
+        return new HypotensiveHypoxemiaAlert(patientId, "Hypoxemia: " + condition, timestamp);
+    }
+}
