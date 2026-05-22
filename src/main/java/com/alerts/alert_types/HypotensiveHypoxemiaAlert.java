07a2e75162ee1e408ab2e210ad277c2a8d88ef63
NS11235 <n.simionescu@student.maastrichtuniversity.nl>
2026-05-22 23:38:07 +0200

    Update HypotensiveHypoxemiaAlert.java

--- src/main/java/com/alerts/alert_types/HypotensiveHypoxemiaAlert.java
+++ src/main/java/com/alerts/alert_types/HypotensiveHypoxemiaAlert.java
@@ -1,5 +1,7 @@
 package com.alerts.alert_types;
-
+/**
+ * Alert for hypotensivity and hypoxemia.
+ */
 public class HypotensiveHypoxemiaAlert extends AlertClassic implements Alert {
 
     public HypotensiveHypoxemiaAlert(int patientId, String condition, long timestamp) {
