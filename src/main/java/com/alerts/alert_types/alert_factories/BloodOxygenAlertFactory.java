158dc06036ec5e56ac4a6e9adc962b2afca0524b
NS11235 <n.simionescu@student.maastrichtuniversity.nl>
2026-05-22 20:04:16 +0200

    Update BloodOxygenAlertFactory.java

--- src/main/java/com/alerts/alert_types/alert_factories/BloodOxygenAlertFactory.java
+++ src/main/java/com/alerts/alert_types/alert_factories/BloodOxygenAlertFactory.java
@@ -1,11 +1,12 @@
 package com.alerts.alert_types.alert_factories;
 
 import com.alerts.alert_types.AlertClassic;
+import com.alerts.alert_types.BloodSaturationAlert;
 
 public class BloodOxygenAlertFactory extends AlertFactory {
     @Override
-    public AlertClassic createAlert(int patientId, String condition, long timestamp)
+    public AlertClassic createAlert(int patientId, String condition, long timestamp, double value)
     {
-        return new AlertClassic(patientId, "Blood oxygen: " + condition, timestamp);
+        return new BloodSaturationAlert(patientId, "Blood oxygen: " + condition, timestamp, value);
     }
 }
